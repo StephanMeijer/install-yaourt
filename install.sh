a7a47abfe14779ee15dd2019a153d69a33d1c10d
Stephan Meijer <me@stephanmeijer.com>
2015-11-28 13:20:33 +0100

    Now checking if packages are already installed

--- install.sh
+++ install.sh
@@ -1,11 +1,18 @@
-sudo pacman -S base-devel yajl
+#!/bin/sh
 
-wget https://aur.archlinux.org/cgit/aur.git/snapshot/package-query.tar.gz  # download source tarball
-tar xfz package-query.tar.gz
-cd package-query  &&  makepkg
-sudo pacman -U package-query*.pkg.tar.xz
+if hash yaourt 2>/dev/null; then
+    echo Yaourt is already installed
+else
+    pacman -Ss base-devel || sudo pacman -S base-devel
+    pacman -Ss yajl || sudo pacman -S yajl
 
-wget https://aur.archlinux.org/cgit/aur.git/snapshot/yaourt.tar.gz
-tar xzf yaourt.tar.gz
-cd yaourt  &&  makepkg
-sudo pacman -U yaourt*.pkg.tar.xz
+    wget https://aur.archlinux.org/cgit/aur.git/snapshot/package-query.tar.gz  # download source tarball
+    tar xfz package-query.tar.gz
+    cd package-query  &&  makepkg
+    sudo pacman -U package-query*.pkg.tar.xz
+    
+    wget https://aur.archlinux.org/cgit/aur.git/snapshot/yaourt.tar.gz
+    tar xzf yaourt.tar.gz
+    cd yaourt  &&  makepkg
+    sudo pacman -U yaourt*.pkg.tar.xz
+fi
